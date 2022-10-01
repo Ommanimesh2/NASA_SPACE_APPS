5a18b115b060b244cbc98f62feaf9e21a7702cc2
Ommanimesh2 <93693087+Ommanimesh2@users.noreply.github.com>
2022-10-01 14:14:05 +0530

    sun dekho

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState } from 'react'
 
 function App() {
+  const [div,setdiv]=useState('')
+  const parent =(e)=>{
+setdiv(e.target)
+  }
+const handleclick=(e)=>{
+  console.log(e.target);
+e.target.style.backgroundImage="url(https://svs.gsfc.nasa.gov/vis/a030000/a030300/a030362/euvi_aia304_2012_carrington_print.jpg)"
+div.style.height="450px"
+div.style.width="450px"
+div.style.top="40vh"
+div.style.bottom="45vw"
+
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div onClick={parent} className="earth">
+    <div onClick={handleclick}></div>
+  </div>
   );
 }
 
