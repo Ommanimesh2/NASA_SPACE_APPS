4402466e65e5ead243165a8fdac642cf0e493055
Priyanshu_M <p_mehra@ce.iitr.ac.in>
2022-10-02 19:06:05 +0530

    Update SpeedoMeter.jsx

--- src/components/speedometer/SpeedoMeter.jsx
+++ src/components/speedometer/SpeedoMeter.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import dataContext from '../../datacontext'
+import { useContext } from 'react'
+import Arc from './Arc';
+import { useState } from 'react'; 
+// import ReactSpeedometer from "react-d3-speedometer"
+import ReactDOM from "react-dom";
+function speedmeter() {
+  const [value,setValue]=useState(50)
+  const {globalData}=useContext(dataContext);
 
-const SpeedoMeter = () => {
   return (
     <div>
-      speedometer
+        <div className="container1">
+          <Arc value={globalData.protonSpeed}/>
+        </div>
+      {/* <ReactSpeedometer 
+       value={this.this.props.value}
+       needleTransitionDuration={4000}
+       currentValuePlaceholderStyle="#{value}"
+       needleTransition="easeElastic"
+       currentValueText="Current Value:#{value}" segments={1000} maxSegmentLabels={100}/> */}
+      
     </div>
   )
 }
 
-export default SpeedoMeter
+export default speedmeter
